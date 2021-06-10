9c621385161f1c51c0f12aefd8486152e543e82f
AndrewE1234 <andrew@omnicode.com>
2021-06-09 20:17:16 -0400

    create add button for tasks

--- app/src/main/java/com/andrewe/taskmanager/MainActivity.java
+++ app/src/main/java/com/andrewe/taskmanager/MainActivity.java
@@ -3,12 +3,26 @@ package com.andrewe.taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.addTaskFloatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
