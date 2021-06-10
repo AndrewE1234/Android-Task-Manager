81b78b814a24d378f52023525e96a63d760740a1
AndrewE1234 <andrew@omnicode.com>
2021-06-09 21:05:29 -0400

    added Task class

---
+++ app/src/main/java/com/andrewe/taskmanager/Task.java
@@ -0,0 +1,13 @@
+package com.andrewe.taskmanager;
+
+public class Task {
+    private String taskName;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+}
