1790e3552cadf4432b0bfd8c84eeadbbf4ed7d30
AndrewE1234 <andrew@omnicode.com>
2021-06-09 21:12:47 -0400

    added taskList to adapter

--- app/src/main/java/com/andrewe/taskmanager/TasksRecyclerAdapter.java
+++ app/src/main/java/com/andrewe/taskmanager/TasksRecyclerAdapter.java
@@ -1,32 +1,47 @@
 package com.andrewe.taskmanager;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TasksRecyclerAdapter extends RecyclerView.Adapter {
+public class TasksRecyclerAdapter extends RecyclerView.Adapter<TasksRecyclerAdapter.TaskViewHolder> {
+    private List<Task> taskList;
+
+    public TasksRecyclerAdapter (List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_list_item, parent, false);
+
+        return new TaskViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        Task task = taskList.get(position);
+        holder.taskName.setText(task.getTaskName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return taskList.size();
     }
 
     public static class TaskViewHolder extends RecyclerView.ViewHolder {
+        public TextView taskName;
 
         public TaskViewHolder(@NonNull View itemView) {
             super(itemView);
+            taskName = itemView.findViewById(R.id.task_name);
         }
     }
 }
