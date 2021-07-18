282d26cb3fb0f42bf433b03b35a88da55b1d6bd6
AndrewE1234 <andrew@omnicode.com>
2021-07-18 18:10:14 -0400

    Set date form to current date for testing

--- app/src/main/java/com/andrewe/taskmanager/TaskFormActivity.java
+++ app/src/main/java/com/andrewe/taskmanager/TaskFormActivity.java
@@ -1,10 +1,17 @@
 package com.andrewe.taskmanager;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class TaskFormActivity extends AppCompatActivity {
 
@@ -12,5 +19,13 @@ public class TaskFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_form);
+
+        // FOR TESTING ONLY!!
+        // REMOVE IN THE FUTURE!!
+        EditText dateEditText = findViewById(R.id.date_edit_text);
+
+        Date currentDate = new Date();
+        String currentDateString = new SimpleDateFormat("MM/dd/yyyy").format(currentDate);
+        dateEditText.setText(currentDateString);
     }
 }
---
+++ app/src/main/java/com/andrewe/taskmanager/TaskManagerContract.java
@@ -0,0 +1,2 @@
+package com.andrewe.taskmanager;public class TaskManagerContract {
+}
